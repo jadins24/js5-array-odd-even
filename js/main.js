3cb5006ced9aaf8b0ec4984f8c5a016da6d21faf
jadins24 <jadinesh04@gmail.com>
2022-12-21 09:53:44 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var a=[1,2,3,4,5,6,7];
+var b=[];
+var c=[];
+var d=0;
+var e=0;
+
+
+for(i=0; i<a.length ;i++){
+    if(a[i]%2==0){
+        b[d]=a[i]
+        d++
+    }
+    else{
+        c[e]=a[i]
+        e++
+    }
+
+}
+    document.write(b+"<br>");
+        document.write(c)
+
